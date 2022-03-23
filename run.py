9c7429afa02560ac62f381476d38820bb96100b8
HaimanotA <haimiayana530@gmail.com>
2022-03-23 19:21:12 +0000

    Create a variable which takes an input from the user

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import numpy_financial as npf
+# Loan calculator and amortization schedule
+principal = int(input("Enter the principal amount: "))
+interest_rate = float(input("Enter the interest rate amount: "))
+term_in_years = int(input("Enter term in years: "))
+# initiation_date = input ("Enter initiation date: ")
+
+# print(principal)
+# print(interest_rate)
+# print(term_in_years)
+# print(initiation_date)
+
+
+# p the principal amount
+# r interest rate
+# number of years
+
+
+def calculate_amortization_amount(p, r, n):
+    amt = npf.pmt(r/12, n*12, p)
+    print(round(amt, 2))
+
+
+calculate_amortization_amount(principal, interest_rate, term_in_years)
